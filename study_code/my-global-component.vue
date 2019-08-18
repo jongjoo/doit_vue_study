810585d5667a8242504917500f8b525466eddd2a
김종주 <jongjoo@lfcorp.co.kr>
2019-08-18 23:12:03 +0900

    Last Commit. 웹팩 사용 필요함.

---
+++ study_code/my-global-component.vue
@@ -0,0 +1,14 @@
+<!-- Webpack의 필요성이 나타났다.. -->
+<template lang="html">
+  <div>
+    <p>전역 컴포넌트가 등록되었습니다.</p>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
